d0c192dcbc4b8c29c8822bed471959dfcea98259
ShijieTang <shijie.20@intl.zju.edu.cn>
2021-03-17 10:19:06 +0800

    practical6

--- Practical6/average_exon_length.py
+++ Practical6/average_exon_length.py
@@ -2,6 +2,7 @@ import numpy as np
 gene_lengths = np.array([9410,394141,4442,105338,19149,76779,126550,36296,842,15981])
 exon_counts = np.array([51,1142,42,216,25,650,32533,57,1,523])
 average_lengths = gene_lengths/exon_counts
+average_lengths.sort()
 print (average_lengths)
 
 import matplotlib.pyplot as plt
