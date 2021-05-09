003001f51081b0e31ee104cdaceab8b22560a925
ShijieTang <shijie.20@intl.zju.edu.cn>
2021-05-09 23:29:28 +0800

    optimize Practical8

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -12,15 +12,14 @@ for line in file:
    gene_name = ' '
    a = False
   if re.findall(r'unknown', line):
-   gene_name = re.findall(r'^>.+?_', line)
+   gene_name = re.findall(r'^>(.+?)_', line)
    gene_name = gene_name[0]
-   gene_name = gene_name[:-1]
    a = True
  else:
   if a == True:
    gene = gene +line.strip()+'\n'
 file.close()
-result_output = open('unknown_function.fa','w')
+result_output = open('unknown_function(1).fa','w')
 result_output.write(result)
 result_output.close()
 
