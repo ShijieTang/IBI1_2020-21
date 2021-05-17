3a38d465cd3d2acd826bf393c907e896f56a976f
ShijieTang <shijie.20@intl.zju.edu.cn>
2021-05-17 14:52:54 +0800

    The second version

--- Practical14/count_childNodes.py
+++ Practical14/count_childNodes.py
@@ -3,6 +3,7 @@ import xml.dom.minidom
 import re
 import matplotlib.pyplot as plt
 
+# Define the variable 
 count_DNA = 0
 count_RNA = 0
 count_Protein = 0
@@ -10,12 +11,12 @@ count_Carbohydrate = 0
 count_Oligosaccharide = 0
 son_namelist = []
 
-# in put the file and extract "term"
+# Input the file and extract "term"
 file = xml.dom.minidom.parse("go_obo.xml")
 collection = file.documentElement
 terms = collection.getElementsByTagName("term")
 
-# write a function to count
+# Write a function to count the number.
 def counter(x):
  print("============= "+ x +" =============")
  n = 0
@@ -35,6 +36,7 @@ def counter(x):
          fathers_id.append(father_id.childNodes[0].data)
  print("Parents have been found")
  print("The number is : " + str(len(fathers_id)))
+ print("")
  print(x+ " sons are been founding ...")
  for term in terms:
      if term.getElementsByTagNameNS('*','is_a') == []:
@@ -49,12 +51,13 @@ def counter(x):
                      break
  print("The primary sons have been found")
  print("The number is : " + str(len(son_namelist)))
+ print("")
  print("Other sons are been finding ...")
  while len(son_namelist1) != len(son_namelist2):
      n +=1
+     print("Counting ... " + '\n'+ "## Please be patient, Thank you! (^-^) ##")
      print("Checking version is : " + str(n))
      son_namelist1 = list(son_namelist)
-     print("son_namelist1 is : (before) " + str(len(son_namelist1)))
      for term in terms:
          if term.getElementsByTagNameNS(term.namespaceURI,'is_a') == []:
              continue
@@ -67,14 +70,13 @@ def counter(x):
                      break
      son_namelist = list(set(son_namelist))
      son_namelist2 = son_namelist
-     print("son_namelist1 is : (after) " + str(len(son_namelist1)))
-     print("son_namelist2 is " + str(len(son_namelist2)))
-     print(len(son_namelist))
+     print("The number now is : "+ str(len(son_namelist)))
+     print("")
  print("Congratulation! Search is finished.")
  print("The "+x+" number is %s" % len(son_namelist))
  return son_namelist
 
-# find the specific item we want
+# Get the number of specific items
 x = "DNA"
 counter(x)
 count_DNA = len(son_namelist)
@@ -91,7 +93,15 @@ x = "oligosaccharide"
 counter(x)
 count_Oligosaccharide = len(son_namelist)
 
-# draw the pie chart
+# Show the result
+print("Here is the result: ")
+print("The DNA number is %s" % count_DNA)
+print("The Protein number is %s" % count_Protein)
+print("The RNA number is %s" % count_RNA)
+print("The Carbohydrate number is %s" % count_Carbohydrate)
+print("The Oligosaccharide number is %s" % count_Oligosaccharide)
+        
+# Draw the pie chart
 explode = (0, 0, 0, 0.1, 0)
 number = (count_DNA, count_Protein, count_Oligosaccharide,count_RNA, count_Carbohydrate,)
 labels = ("count_DNA", "count_Protein", "count_Oligosaccharide", "count_RNA", "count_Carbohydrate")
@@ -99,10 +109,3 @@ plt.pie(number, explode=explode,labels=labels, autopct='%1.1f%%', shadow=True, s
 plt.axis('equal')
 plt.title('The proportion of the macromolecule number')
 plt.show()
-
-# show the result
-print("the DNA number is %s" % count_DNA)
-print("the Protein number is %s" % count_Protein)
-print("the RNA number is %s" % count_RNA)
-print("the Carbohydrate number is %s" % count_Carbohydrate)
-print("the Oligosaccharide number is %s" % count_Oligosaccharide)
