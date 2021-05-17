7c8ade0c595af1ccc147cf9415abd7f1b1c0794e
ShijieTang <shijie.20@intl.zju.edu.cn>
2021-05-17 21:37:02 +0800

    version 3

--- Practical8/unknown_DNA_to_protein.py
+++ Practical8/unknown_DNA_to_protein.py
@@ -1,13 +1,13 @@
 import re
 protein = ''
-file = input('')
-code = {'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+file = input('Please input the file name :')
+code = {'ATA': 'J', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
         'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
-        'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+        'AAC': 'B', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
         'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
         'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
         'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
-        'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Z',
         'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
         'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
         'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
@@ -15,8 +15,8 @@ code = {'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
         'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
         'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
         'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
-        'TAC': 'Y', 'TAT': 'Y', 'TAA': '', 'TAG': '',
-        'TGC': 'C', 'TGT': 'C', 'TGA': '', 'TGG': 'W'}
+        'TAC': 'Y', 'TAT': 'Y', 'TAA': 'O', 'TAG': 'U',
+        'TGC': 'C', 'TGT': 'C', 'TGA': 'X', 'TGG': 'W'}
 file_gene = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
 gene_name=''
 result=''
@@ -28,9 +28,10 @@ for line in file_gene:
    for n in range(0,len(gene),3):
     if gene[n:n+3] in code:
      protein = protein + code[gene[n:n+3]]
-   result = result+gene_name+'     '+ str(len(gene))+ '\n' + protein
+   result = result+gene_name+'     '+ str(len(protein))+ '\n' + protein + '\n'
    gene = ''
    gene_name=''
+   protein = ''
    a = False
   if re.findall(r'unknown', line):
    gene_name = re.findall(r'(^>.+?)_', line)
@@ -40,8 +41,8 @@ for line in file_gene:
   if a == True:
    gene = gene + line.strip()
 file_gene.close()
-result_output = open(file, 'w')
+result_output = open(file +".fa", 'w')
 result_output.write(result)
 result_output.close()
-
+print("file is successully created")
 
