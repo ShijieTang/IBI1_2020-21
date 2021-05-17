21dc1b18784b5cdf6c3799ac8c8723aaece51f5c
ShijieTang <shijie.20@intl.zju.edu.cn>
2021-05-17 23:09:48 +0800

    optimized

--- Practical6/COVID-19.py
+++ Practical6/COVID-19.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 # set numbers of each country's population
 Country = ['USA', 'India', 'Brazil', 'Russia', 'UK']
 People = {'USA':29862124, 'India':11285561, 'Brazil':11205972, 'Russia':4360823, 'UK':4234924}
-number = [29862124, 11285561, 11205972, 4360823, 4234924]
+number = [People['USA'], People['India'], People['Brazil'], People['Russia'], People['UK']]
 # draw the pie chart
 explode = (0, 0, 0.1, 0, 0)
 plt.pie(number, explode=explode,labels=Country, autopct='%1.1f%%', shadow=True, startangle=90)
